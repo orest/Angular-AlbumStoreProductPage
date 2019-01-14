15fb460e5e5776dadbd38a038a6b3095d91b8412
Orest DEV-10 <orest@proficiencyconsulting.com>
2019-01-14 01:05:01 -0500

    asdf

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import{map, filter} from "rxjs/operators"
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import  'rxjs/add/operator/map';
 
 @Injectable()
