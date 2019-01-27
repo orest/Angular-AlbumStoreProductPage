db46956ceea7ce925799b2587c74b890486dc3e7
Orest <orest@proficiencyconsulting.com>
2019-01-27 16:02:37 -0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
@@ -11,22 +12,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+const appRoutes: Routes = [
+    { path: "products", component: ProductListComponent },
+    { path: "product/:id", component: ProductPageComponent },
+    { path: "", redirectTo: "products", pathMatch: "full" },
+
+]
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductPageComponent,
-    ProductDescriptionComponent,
-    ProductTracklistingComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-	HttpModule,
-	HttpClientModule
-  ],
-  providers: [ProductService],
-  
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductPageComponent,
+        ProductDescriptionComponent,
+        ProductTracklistingComponent,
+        ProductListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        HttpClientModule,
+        RouterModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [ProductService],
+
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
